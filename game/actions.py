c825961783b61dc4c670383f20e1ac9eaac228f0
virtuoso-rotins <>
2021-07-10 20:25:16 +0200

    adapted code and structure to 2021 tutorial

---
+++ game/actions.py
@@ -0,0 +1,39 @@
+"""Holds the different types of actions the player can perform"""
+from __future__ import annotations
+# __future__ annotations helps reduce issues from modules referencing each other which can happen often
+# whenever type-hinting is being used.
+
+import game.entity
+
+
+class Action:
+    def __init__(self, entity: game.entity.Entity) -> None:
+        super().__init__()
+        self.entity = entity  # The object performing the action.
+        self.engine = entity.gamemap.engine
+
+    def perform(self) -> None:
+        """Perform this action now.
+
+        This method must be overridden by Action subclasses.
+        """
+        raise NotImplementedError()
+
+
+class Move(Action):
+    def __init__(self, entity: game.entity.Entity, dx: int, dy: int):
+        super().__init__(entity)
+
+        self.dx = dx
+        self.dy = dy
+
+    def perform(self) -> None:
+        dest_x = self.entity.x + self.dx
+        dest_y = self.entity.y + self.dy
+
+        if not self.engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds.
+        if not self.engine.game_map.tiles[dest_x, dest_y]:
+            return  # Destination is blocked by a tile.
+
+        self.entity.x, self.entity.y = dest_x, dest_y
---
+++ game/engine.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import game.entity
+import game.game_map
+
+
+class Engine:
+    """
+    The Engine class is missing its constructor method __init__ which will normally be defined in all other classes.
+    It is excluded here because the objects held by Engine will need Engine to already exist for they can be created
+    """
+    game_map: game.game_map.GameMap
+    player: game.entity.Entity
+
+    def handle_events(self, events: Iterable[Any]) -> None:
+        for event in events:
+            action = self.event_handler.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform(self, self.player)
+
+    def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+
+        for entity in self.entities:
+            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+
+        context.present(console)
+
+        console.clear()
